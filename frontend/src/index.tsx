2a6d22acc81ecfa105ccd77a11a969f894e9717c
dean <deanfernandes18@gmail.com>
2025-08-01 17:13:50 +0100

    Add postcss; Add tailwindcss

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './style.css';
 
 const App: React.FC = () => (
-  <h1>Hello, React + TypeScript + Webpack!</h1>
+  <h1 className='font-bold text-blue-600 text-3xl underline'>Hello, React + TypeScript + Webpack + Tailwind CSS!</h1>
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
